94b90aa4799e34a3ae8a39a8556d4d5fa251acf2
Nicholas <n33kos@gmail.com>
2016-04-13 22:06:00 -0600

    Implemented behavior for model, controller, and view.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,57 @@
 class EventsController < ApplicationController
 	def index
 		@events = Event.all
+		@dates =  Event.unique_dates
+		@tracks = Event.uniq.pluck(:track)
+		
+		if !params[:sort].blank?
+			if params[:sort] == "alpha"
+				@tracks = @tracks.sort{|a,b| a<=>b}
+			else
+				@tracks = @tracks.sort{|a,b| b<=>a}
+			end
+		end
+
+		@global_events = [
+			"Monday Plenary Session 1",
+			"Coffee Break",
+			"Lunch",
+			"Poster Session MoBT8",
+			"A night at the acquarium",
+		]
+		@excluded_tracks = [
+			"Keynote",
+			"Coffee Breaks",
+			"Party",
+		]
 		#csvimport #used for database population
 	end
 
 	def bydate
-		@events = Event.bydate({:date => params[:date], :sortby => params[:sort]})
+		@events = Event.by_date({:date => params[:date], :sort => params[:sort]})
+		@dates =  Event.unique_dates
+		@tracks = Event.uniq.pluck(:track)
+		
+		if !params[:sort].blank?
+			if params[:sort] == "alpha"
+				@tracks = @tracks.sort{|a,b| a<=>b}
+			else
+				@tracks = @tracks.sort{|a,b| b<=>a}
+			end
+		end
+
+		@global_events = [
+			"Monday Plenary Session 1",
+			"Coffee Break",
+			"Lunch",
+			"Poster Session MoBT8",
+			"A night at the acquarium",
+		]
+		@excluded_tracks = [
+			"Keynote",
+			"Coffee Breaks",
+			"Party",
+		]
 	end
 
 	def csvimport
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,17 @@
 class Event < ActiveRecord::Base
+	def self.unique_dates
+		unique_dates = Event.uniq.pluck(:start)
+		parsed_dates = []
+		unique_dates.each do |date|
+			parsed_dates.push(Date.parse(date.to_s))
+		end
+		return parsed_dates.uniq
+	end
+
+	def self.by_date(options = {})
+		limit_date = Date.parse(options[:date])
+		results = Event.where(:start => limit_date.beginning_of_day..limit_date.end_of_day)
+		return results
+	end
+
 end
